469c834f384b8134dbc265e7fc8ce453e7a61578
shaantalk <shaantalk@gmail.com>
2021-05-23 02:47:44 +0530

    Added setup code

---
+++ src/tests/test_human_readable_time.py
@@ -0,0 +1,20 @@
+import unittest
+from youtubeTask import *
+
+
+class TestHumanReadableTime(unittest.TestCase):
+    # Test human_readable_time function for only seconds
+    def test_human_readable_time_only_seconds(self):
+        expression = human_readable_time(50)
+        expectedValue = '50 secs'
+        failMessage = 'Test Failed : human_readable_time function for only seconds'
+
+        self.assertEqual(expression, expectedValue, failMessage)
+
+    # Test human_readable_time function for days
+    def test_human_readable_time_days(self):
+        expression = human_readable_time(8000000)
+        expectedValue = '92 days 14 hours 13 mins 20 secs'
+        failMessage = 'Test Failed : human_readable_time function for days'
+
+        self.assertEqual(expression, expectedValue, failMessage)
